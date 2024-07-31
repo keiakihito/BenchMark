ffa47407f96897f4347668b2a01987ce7389f0ac
keiakihito <kkatsumi.git@gmail.com>
2024-07-31 10:37:43 -0700

    Benchmarked PCG and adding chart example

--- include/functions/pcg.h
+++ include/functions/pcg.h
@@ -19,12 +19,14 @@
 #include "helper.h"
 #include "cuBLAS_util.h"
 #include "cuSPARSE_util.h"
+#include "../struct/BenchMarkPCG.h"
 
 
 //Input:
 //Process: Conjugate Gradient
 //Output: vecSolX
 void pcg(CSRMatrix &csrMtxA, double *vecSolX_h, double *vecB_h, int numOfA);
+void pcg_BenchMark(CSRMatrix &csrMtxA, double *vecSolX_d, double *vecB_d, int numOfA, BenchMarkPCG& bmPCG);
 
 
 void pcg(CSRMatrix &csrMtxA, double *vecSolX_d, double *vecB_d, int numOfA)
@@ -255,5 +257,195 @@ void pcg(CSRMatrix &csrMtxA, double *vecSolX_d, double *vecB_d, int numOfA)
     CHECK(cudaFree(q_d););
     
 }// end of pcg
+
+
+void pcg_BenchMark(CSRMatrix &csrMtxA, double *vecSolX_d, double *vecB_d, int numOfA, BenchMarkPCG& bmPCG)
+{
+    double startTime, endTime;
+    bool debug = false;
+    const double THRESHOLD = 1e-6;
+
+    double *r_d = NULL; // Residual
+    double *s_d = NULL; // For s <- M * r and delta <- r' * s
+    CSRMatrix csrMtxM = generateSparseIdentityMatrixCSR(numOfA); // Precondtion
+    double *dirc_d = NULL; // Direction
+    double *q_d = NULL; // Vector Ad
+    double dot = 0.0f; // temporary val for d^{T} *q to get aplha
+    
+    //Using for cublas functin argument
+    double alpha = 1.0; 
+
+
+    // double initial_delta = 0.0;
+    double delta_new = 0.0;
+    double delta_old = 0.0;
+    // double relative_residual = 0.0;
+
+    // In CG iteration alpha and beta
+    double alph = 0.0f;
+    double ngtAlph = 0.0f;
+    double bta = 0.0f;
+
+    
+
+    //Crete handler
+    cublasHandle_t cublasHandler = NULL;
+    cusparseHandle_t cusparseHandler = NULL;
+    // cusolverDnHandle_t cusolverHandler = NULL;
+    
+    CHECK_CUBLAS(cublasCreate(&cublasHandler));
+    CHECK_CUSPARSE(cusparseCreate(&cusparseHandler));
+    // CHECK_CUBLAS(cusolverDnCreate(&cusolverHandler));
+
+    //(1) Allocate space in global memory
+    CHECK(cudaMalloc((void**)&r_d, sizeof(double) * numOfA));
+    CHECK(cudaMalloc((void**)&s_d, sizeof(double) * numOfA));
+    CHECK(cudaMalloc((void**)&dirc_d, sizeof(double) * numOfA));
+    CHECK(cudaMalloc((void**)&q_d, sizeof(double) * numOfA));
+
+    //(2) Copy from host to device
+    CHECK(cudaMemcpy(r_d, vecB_d, sizeof(double) * numOfA, cudaMemcpyDeviceToDevice));
+
+
+
+    //(5) Iteration
+    /* 💫💫💫Begin CG💫💫💫 */
+    //Setting up the initial state.
+
+
+    //r = b - Ax
+    den_vec_subtract_multiplly_Sprc_Den_vec(cusparseHandler, csrMtxA, vecSolX_d, r_d);
+
+    //Set d <- M * r;
+    //M is Identity matrix for the place holder of precondition
+    // CHECK(cudaMemcpy(dirc_d, r_d, N * sizeof(double), cudaMemcpyDeviceToDevice));
+    multiply_Sprc_Den_vec(cusparseHandler, csrMtxM, r_d, dirc_d);
+
+
+    //delta_{new} <- r^{T} * d
+    // Compute the squared norm of the initial residual vector r (stored in r1).
+    CHECK_CUBLAS(cublasDdot(cublasHandler, numOfA, r_d, 1, dirc_d, 1, &delta_new));
+    //Save it for the relative residual calculation.
+    // initial_delta = delta_new;
+
+    
+    int counter = 1; // counter
+    const int MAX_ITR = 100;
+
+    while(counter <= MAX_ITR){
+
+        // printf("\n\n💫💫💫= = = Iteraion %d= = = 💫💫💫\n", counter);
+
+        
+        //q <- Ad
+        startTime = myCPUTimer();
+        multiply_Sprc_Den_vec(cusparseHandler, csrMtxA, dirc_d, q_d);
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.q_ap_times.push_back(endTime - startTime);
+        }        
+
+        //dot <- d^{T} * q
+        startTime = myCPUTimer();
+        CHECK_CUBLAS(cublasDdot(cublasHandler, numOfA, dirc_d, 1, q_d, 1, &dot));
+
+        //alpha(a) <- delta_{new} / dot // dot <- d^{T} * q 
+        alph = delta_new / dot;
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.alpha_times.push_back(endTime - startTime);
+        }
+
+
+        //x_{i+1} <- x_{i} + alpha * d_{i}
+        startTime = myCPUTimer();
+        CHECK_CUBLAS(cublasDaxpy(cublasHandler, numOfA, &alph, dirc_d, 1, vecSolX_d, 1));
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.x_update_times.push_back(endTime - startTime);
+        }
+
+
+
+        if(counter % 50 == 0){
+            //r <- b -Ax Recompute
+            CHECK(cudaMemcpy(r_d, vecB_d, sizeof(double) * numOfA, cudaMemcpyHostToDevice));
+            den_vec_subtract_multiplly_Sprc_Den_vec(cusparseHandler, csrMtxA, vecSolX_d, r_d);
+            if(debug){
+                printf("\n\nr_{0} = \n");
+                print_vector(r_d, numOfA);
+            }
+        }else{
+            //r_{i+1} <- r_{i} -alpha*q
+            startTime = myCPUTimer();
+            ngtAlph = -alph;
+            CHECK_CUBLAS(cublasDaxpy(cublasHandler, numOfA, &ngtAlph, q_d, 1, r_d, 1));
+            endTime = myCPUTimer();
+            if(counter == 1){
+                bmPCG.r_update_times.push_back(endTime - startTime);
+            }
+            
+        }
+
+        //s <- M * r
+        startTime = myCPUTimer();
+        multiply_Sprc_Den_vec(cusparseHandler, csrMtxM, r_d, s_d);
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.s_update_times.push_back(endTime - startTime);
+        }
+
+        // delta_old <- delta_new
+        delta_old = delta_new;
+
+        // delta_new <- r' * s
+        // bta <- delta_new / delta_old
+        startTime = myCPUTimer();
+        CHECK_CUBLAS(cublasDdot(cublasHandler, numOfA, r_d, 1, s_d, 1, &delta_new));
+        bta = delta_new / delta_old;
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.beta_times.push_back(endTime - startTime);
+        }
+
+
+        // relative_residual = sqrt(delta_new)/sqrt(initial_delta);
+        // printf("\n\n🫥Relative residual = %f🫥\n", relative_residual);
+       
+        if(sqrt(delta_new) < THRESHOLD){
+            // printf("\n\n🌀🌀🌀CONVERGED🌀🌀🌀\n\n");
+            break; 
+        }
+        
+
+        //d <- s + ßd
+        startTime = myCPUTimer();
+        CHECK_CUBLAS(cublasDscal(cublasHandler, numOfA, &bta, dirc_d, 1)); //d <- ßd
+        CHECK_CUBLAS(cublasDaxpy(cublasHandler, numOfA, &alpha, s_d, 1, dirc_d, 1)); // d <- s + d
+        endTime = myCPUTimer();
+        if(counter == 1){
+            bmPCG.d_update_times.push_back(endTime - startTime);
+        }
+
+
+        counter++;
+    } // end of while
+
+
+
+
+    //(6) Free the GPU memory after use
+    CHECK_CUBLAS(cublasDestroy(cublasHandler));
+    CHECK_CUSPARSE(cusparseDestroy(cusparseHandler));
+    CHECK(cudaFree(r_d));
+    CHECK(cudaFree(s_d));
+    CHECK(cudaFree(dirc_d));
+    CHECK(cudaFree(q_d););
+    
+}// end of pcg
+
+
+
+
 
 #endif // PCG_H
